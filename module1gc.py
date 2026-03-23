2160059364ef1fc3d0a0588c5d34d71f1f7c12d2
Gargi Chakraborty <gargi.chakraborty.187@gmail.com>
2026-03-23 08:25:36 -0700

    Implement GC percentage calculation function
    Added a function to compute GC percentage from a DNA sequence, handling both lowercase and uppercase bases.

--- module1gc.py
+++
@@ -1,8 +0,0 @@
-#This is my first Python program
-# get DNA sequence:
-dna=input("Enter a DNA sequence, please:") 
-no_c = dna.count('c') # count C's in DNA sequence
-no_g = dna.count('g') # count G's in DNA sequence
-dna_length = len(dna) # get the length of the DNA sequence 
-gc_percent = (no_c+no_g)*100/dna_length # compute GC percentage
-print("The DNA sequence's GC content is %5.4f%%:"%gc_percent) # print GC% to screen
